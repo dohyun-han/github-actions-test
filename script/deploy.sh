f95e3a9c45ee83a1980dc73c66ed4f93101b4e55
dohyeon-han <limited.hyeon@gmail.com>
2023-01-06 12:21:41 +0900

    deploy.sh down 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,10 +5,12 @@ echo "> image pull"
 # pull image
 docker pull ghcr.io/dohyeon-han/github-actions-test:latest
 
-echo "> docker compose 재실행"
+echo "> docker container down"
 
 # docker down
-docker-compose -f docker-compose.prod.yml down --rmi all --remove-orphans
+docker-compose -f docker-compose.prod.yml down --remove-orphans
+
+echo "> docker container up"
 
 # docker up
 docker-compose -f docker-compose.prod.yml up -d --build
