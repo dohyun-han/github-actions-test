28e656d357a03e481248dac5d51d7aa0bc9c33cb
dohyeon-han <limited.hyeon@gmail.com>
2023-01-06 11:59:18 +0900

    deploy.sh 오타 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-echo "> docker compose 재실행"
+echo "> image pull"
 
 # pull image
 docker pull ghcr.io/dohyeon-han/github-actions-test:latest
 
+echo "> docker compose 재실행"
+
 # docker down
-docker compose -f docker-compose.prod.yml down --rmi all --remove-orphans
+docker-compose -f docker-compose.prod.yml down --rmi all --remove-orphans
 
 # docker up
-docker compose -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml up -d --build
