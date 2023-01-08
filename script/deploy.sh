cf72dc116e1a10e18ad72a1ed382f74857d5a420
dohyeon-han <limited.hyeon@gmail.com>
2023-01-08 17:18:28 +0900

    ci port 수정 및 이전 이미지 삭제

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,11 @@
 echo "> run MySQL, redis and nginx container"
 docker-compose -f docker-compose.prod.yml up mysql redis nginx -d --build
 
-echo "> docker container up"
+echo "> docker spring container down"
+docker-compose -f docker-compose.prod.yml rm -s -v -f spring1
 
-# docker up
+echo "> docker image remove"
+docker rmi ghcr.io/dohyeon-han/github-actions-test:latest
+
+echo "> docker spring container up"
 docker-compose -f docker-compose.prod.yml up spring1 -d --build
