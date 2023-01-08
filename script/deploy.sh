5bd2fb10c31ca9e7adc5725bdfabcdbc0f0bc6de
dohyeon-han <limited.hyeon@gmail.com>
2023-01-08 14:40:20 +0900

    db container mysql로 이름 변경

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-echo "> docker container down"
-
-# docker down
-docker-compose -f docker-compose.prod.yml down --remove-orphans
+echo "> run MySQL, redis and nginx container"
+docker-compose -f docker-compose.prod.yml up mysql redis nginx -d --build
 
 echo "> docker container up"
 
 # docker up
-docker-compose -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml up spring1 -d --build
